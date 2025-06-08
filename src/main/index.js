925c16e039d0622986ed23ffb1058de71abde5f9
GiaBao <duonggiabao254@gmail.com>
2025-06-08 23:55:41 +0700

    update icons and modify window creation to include icon for all platforms

--- src/main/index.js
+++ src/main/index.js
@@ -10,7 +10,8 @@ function createWindow() {
         height: 670,
         show: false,
         autoHideMenuBar: true,
-        ...(process.platform === "linux" ? { icon } : {}),
+        icon,
+        // ...(process.platform === "linux" ? { icon } : {}),
         webPreferences: {
             preload: join(__dirname, "../preload/index.js"),
             sandbox: false,
