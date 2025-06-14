1cc5116487965d9c426d749fd548183e1a3fecf4
GiaBao <duonggiabao254@gmail.com>
2025-06-14 22:22:46 +0700

    Add homepage, privacy policy, and terms of service HTML files; update app.js for static file serving

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/auth", authRoutes);
 
