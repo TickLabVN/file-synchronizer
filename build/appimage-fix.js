bd5e14c0a3550332f209aed53f42e91c81ca059f
GiaBao <duonggiabao254@gmail.com>
2025-06-16 00:47:45 +0700

    v1.0.7

---
+++ build/appimage-fix.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import child_process from "child_process";
+
+const appName = "file-synchronizer";
+
+function isLinux(targets) {
+    const re = /AppImage|snap|deb|rpm|freebsd|pacman/i;
+    return !!targets.find((target) => re.test(target.name));
+}
+
+async function afterPack({ targets, appOutDir }) {
+    if (!isLinux(targets)) return;
+    const scriptPath = path.join(appOutDir, appName),
+        script =
+            '#!/bin/bash\n"${BASH_SOURCE%/*}"/' +
+            appName +
+            '.bin "$@" --no-sandbox';
+    new Promise((resolve) => {
+        const child = child_process.exec(`mv ${appName} ${appName}.bin`, {
+            cwd: appOutDir,
+        });
+        child.on("exit", () => {
+            resolve();
+        });
+    }).then(() => {
+        fs.writeFileSync(scriptPath, script);
+        child_process.exec(`chmod +x ${appName}`, { cwd: appOutDir });
+    });
+}
+
+export default afterPack;
